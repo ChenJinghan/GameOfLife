df451d6d2f3a841022cbcb77eb911ce17b7d07ae
ChenJinghan <33058908+ChenJinghan@users.noreply.github.com>
2018-06-09 10:50:47 +0800

    init matrix (cell position is fixed in the first 5 position)

--- src/main/java/funtime/GameOfLife.java
+++ src/main/java/funtime/GameOfLife.java
@@ -3,9 +3,22 @@ package funtime;
 import java.util.Random;
 
 public class GameOfLife {
-    Random random =  new Random();
+    Random random = new Random();
     int size = random.nextInt(100);
     String[][] matrixA = new String[size][size];
+
+    public GameOfLife(int cellCount) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == 0 && j < cellCount) matrixA[i][j] = "*";
+                else matrixA[i][j] = " ";
+            }
+        }
+    }
+
+    public GameOfLife() {
+
+    }
 
     public String[][] getMatrix() {
         return matrixA;
--- src/test/java/funtime/GameOfLifeTest.java
+++ src/test/java/funtime/GameOfLifeTest.java
@@ -11,9 +11,31 @@ public class GameOfLifeTest {
         GameOfLife gameOfLife = new GameOfLife();
 
         // When
-        String [][] matrix = gameOfLife.getMatrix();
+        String[][] matrix = gameOfLife.getMatrix();
 
         // Then
         assertTrue(matrix != null);
+    }
+
+    @Test
+    public void shouldGetInitMatrix() {
+        // Given
+        int size = 5;
+        GameOfLife gameOfLife = new GameOfLife(size);
+
+        // When
+        String[][] matrix = gameOfLife.getMatrix();
+        int count = 0;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix.length; j++) {
+                if (matrix[i][j].equals("*")) {
+                    count++;
+                }
+
+            }
+        }
+
+        // Then
+        assertTrue(count == 5);
     }
 }
